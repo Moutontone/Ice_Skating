c1925cadf52f8edb0834885943e18c65699472ef
Armand VM <g.armand2000@gmail.com>
2023-10-05 21:57:17 +0200

    try dyn prod en python (failed)

--- Ice_Skating.py
+++ Ice_Skating.py
@@ -37,11 +37,11 @@ up, left, down, right = (-1,0), (0,-1), (1, 0), (0, 1)
 Actions = [up, left, down, right]
 
 #pos_start = (5,1)
-pos_start = (1,3)
+pos_start = (2,5)
 dir_start = left
 goal = (1,5)
 p_turn = 0.83
-T = 10
+T = 5
 
 # Using Markoc Decision process M = {S, A, r, P}
 
@@ -49,35 +49,106 @@ T = 10
 
 # movements in order up, left, down, right
 
-# W = [[[-1 for _ in Actions] for _ in range(len(map[0]))] for _ in range(len(map))]
+# dic key : (t, pos, dit) -> val : (reward, action)
 dic = {
-        (goal, up): (1, up),
-        (goal, right): (up, 1),
-        (goal, down): (up, 1),
-        (goal, left): (up, 1),
+        (T, goal, up): (1, up),
+        (T, goal, right): (1, up),
+        (T, goal, down): (1, up),
+        (T, goal, left): (1, up),
         }
 
-memory = np.full((T,7,7,4), -1)
-print(f"{memory[0][0][0][0]}")
+memR = np.full((T+1,7,7,4),-1)
+memA = np.full((T+1,7,7,4),-1)
+mem = np.full((T+1,7,7,4),-1)
+
+# base cases
+for i in range(7):
+    for j in range(7):
+        for a in range(4):
+            if (i,j) == goal:
+                memR[T][i][j][a] = 1
+                memA[T][i][j][a] = 0
+            else:
+                memR[T][i][j][a] = 0
+                memA[T][i][j][a] = 0
+
+def has_val(t, pos, dir):
+    a = 0
+    if dir == left : 
+        a = 1
+    if dir == down : 
+        a = 2
+    if dir == right : 
+        a = 3
+    return memR[t][pos[0]][pos[1]][a] != -1
+
+def get_val(t, pos, dir):
+    a = 0
+    if dir == left : 
+        a = 1
+    if dir == down : 
+        a = 2
+    if dir == right : 
+        a = 3
+    r = memR[t][pos[0]][pos[1]][a]
+    a = memA[t][pos[0]][pos[1]][a]
+    if a == 0:
+        return (r, up)
+    if a == 1:
+        return (r, left)
+    if a == 2:
+        return (r,down)
+    if a == 3:
+        return (r, right)
+
+def set_val(t, pos, dir, v, ac):
+    a = 0
+    if dir == left : 
+        a = 1
+    if dir == down : 
+        a = 2
+    if dir == right : 
+        a = 3
+    memR[t][pos[0]][pos[1]][a] = v
+    if ac == up:
+        memA[t][pos[0]][pos[1]][a] = 0
+    if ac == left:
+        memA[t][pos[0]][pos[1]][a] = 1
+    if ac == down:
+        memA[t][pos[0]][pos[1]][a] = 2
+    if ac == right:
+        memA[t][pos[0]][pos[1]][a] = 3
 
 def policy(pos, dir, t):
     _, a = W(pos, dir, t)
     return a
 
 def W(pos, dir, t):
-    if t == T:
+
+    # base cases
+    if t >= T:
+        # should be useless
         return (reward(pos), up)
+    # situation already computed
+    if has_val(t, pos, dir):
+        return get_val(t, pos, dir)
+     # compute new situation
+    # dead end cases
     if map[pos[0]][pos[1]] == 0:
         return (0, up)
+    # recusive case
+    action_max = up
     max = -1
-    action_max = ()
     for a in Actions:
+        # turning successfuly 
         w1, _ = W((pos[0] + a[0], pos[1] + a[1]), a, t+1)
+        # keep the same direction
         w2, _ = W((pos[0] + dir[0], pos[1] + dir[1]), dir, t+1)
         res = reward(pos) + p_turn * w1 + (1-p_turn) * w2
         if res > max:
              max = res
              action_max = a
+    set_val(t, pos,dir, max, action_max)
     return (max, action_max)
 
 
